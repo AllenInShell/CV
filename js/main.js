645fe8523ca58c2c0ab5eac116eb7830641a4afc
alleninshell <alleninshell@outlook.com>
2016-06-22 23:46:11 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
-$(function(){
+$(function () {
     $('#dowebok').fullpage({
-        navigation: true
+        navigation: true,
+        afterLoad: function (anchorLink, index) {
+            if (index == 2) {
+                $('#tongji').delay(500).animate({
+                    left: '0px'
+                }, 1500, 'easeOutExpo');
+                $('#tsinghua').delay(500).animate({
+                    right: '0px'
+                }, 1500, 'easeOutExpo');
+            }
+            if (index == 3) {
+                $('.section3').find('p').delay(500).animate({
+                    bottom: '0'
+                }, 1500, 'easeOutExpo');
+            }
+            if (index == 4) {
+                $('.section4').find('p').fadeIn(2000);
+            }
+        },
+        onLeave: function (index, direction) {
+            if (index == '2') {
+                $('#tongji').delay(500).animate({
+                    left: '-120%'
+                }, 1500, 'easeOutExpo');
+                $('#tsinghua').delay(500).animate({
+                    right: '-120%'
+                }, 1500, 'easeOutExpo');
+            }
+            if (index == '3') {
+                $('.section3').find('p').delay(500).animate({
+                    bottom: '-120%'
+                }, 1500, 'easeOutExpo');
+            }
+            if (index == '4') {
+                $('.section4').find('p').fadeOut(2000);
+            }
         }
-    );
+
+    });
 });
